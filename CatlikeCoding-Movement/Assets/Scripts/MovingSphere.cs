5062ffcc0a9b0fd9484a8680da1cb084efb52d15
Urii98 <99950184+Urii98@users.noreply.github.com>
2022-07-20 20:23:41 +0200

    Tema Surface Contact finalizado

--- CatlikeCoding-Movement/Assets/Scripts/MovingSphere.cs
+++ CatlikeCoding-Movement/Assets/Scripts/MovingSphere.cs
@@ -17,25 +17,40 @@ public class MovingSphere : MonoBehaviour
 	int maxAirJumps = 0;
 
 	[SerializeField, Range(0f, 90f)]
-	float maxGroundAngle = 25f;
+	float maxGroundAngle = 25f, maxStairsAngle = 50f;
+
+	[SerializeField, Range(0f, 100f)]
+	float maxSnapSpeed = 100f; //la velocidad maxima hassta la que se hace lo de snap, si es mas alta que esto directamente no se hace snap
+
+	[SerializeField, Min(0f)]
+	float probeDistance = 1f;
+
+	[SerializeField]
+	LayerMask probeMask = -1, stairsMask = -1;
 
 	bool desiredJump;
-	int groundContactCount;
+	int groundContactCount, steepContactCount;
 	bool onGround => groundContactCount > 0; //En vez de tener en cuenta si contacta con algo o no, lo que haremos es contar si contacta con mas de una cosa con un entero y en caso de ser así, será true
-
-
+	bool OnSteep => steepContactCount > 0;
 
 	int jumpPhase;
+	int stepsSinceLastGrounded; //esto es para saber cuantos frames de fisicas o algo así que el llama "physics steps", se han dado antes de que onground sea true.
+	int stepsSinceLastJump; //esto lo usaremos para que no se haga snap cuando saltemos porque sino quitaria el upward momentum del salto
 
-	float minGroundDotProduct;
+	float minGroundDotProduct, minStairsDotProduct;
 
 	Vector3 velocity, desiredVelocity;
-	Vector3 contactNormal;
+	Vector3 contactNormal, steepNormal; //contactNormal es para el suelo y steepNormal para los suelos que estan como muy inclinados y son muy verticales o algo asi
 	
+	float GetMinDot (int layer)
+    {
+		return (stairsMask & (1 << layer)) == 0 ? minGroundDotProduct : minStairsDotProduct; //Temario: Surface contact 2.3
+	}
 	void OnValidate()
     {
 		minGroundDotProduct = Mathf.Cos(maxGroundAngle * Mathf.Deg2Rad); //Nosotros queremos hablar de los grados como grados, sin embargo la funcion cos trata radianes, asi que simplemente lo multiplicamos por una funcion para que lo convierta en radianes y ya esta
-    }
+		minStairsDotProduct = Mathf.Cos(maxStairsAngle * Mathf.Deg2Rad);
+	}
     private void Awake()
     {
 		body = GetComponent<Rigidbody>();
@@ -53,6 +68,8 @@ public class MovingSphere : MonoBehaviour
 		desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
 
 		desiredJump |= Input.GetButtonDown("Jump");
+
+		//GetComponent<Renderer>().material.SetColor("_Color", onGround ? Color.black : Color.white);
 	}
 
     private void FixedUpdate()
@@ -72,17 +89,24 @@ public class MovingSphere : MonoBehaviour
 
 	void ClearState()
     {
-		groundContactCount = 0;
-		contactNormal = Vector3.zero;
+		groundContactCount = steepContactCount = 0;
+		contactNormal = steepNormal = Vector3.zero;
     }
 
 	void UpdateState()
     {
+		stepsSinceLastGrounded += 1;
+		stepsSinceLastJump += 1;
 		velocity = body.velocity;
 
-		if(onGround)
+		if(onGround || SnapToGround() || CheckSteepContacts())
         {
+			stepsSinceLastGrounded = 0;
+			if(stepsSinceLastJump > 1)
+            {
 			jumpPhase = 0;
+
+            }
 			if(groundContactCount > 1)
             {
 			contactNormal.Normalize();
@@ -94,14 +118,79 @@ public class MovingSphere : MonoBehaviour
 		}
 	}
 
+	bool SnapToGround() //este metodo lo que hará es que la bola se quede stucked en el suelo 
+	{
+		if(stepsSinceLastGrounded > 1 || stepsSinceLastJump > 2) //cuando se de mas de un frame fisico en el que no estamos en contacto con el suelo retornamos false y por tanto no hacemos que se quede stucked, mientras que cuando solo haya uno si, asi evitamos que se de lo de ghost colliders
+									  // y asi la bola no va haciendo pequeños rebotes si p.e hay dos suelos diferntes juntos y uno solo esta un milimetro por encima del otro
+									  //tampoco dejaremos que se haga snap cuando saltemos y el salto dure mas de dos frames de fisica
+        {
+			return false;
+        }
+
+		float speed = velocity.magnitude;
+		if (speed > maxSnapSpeed)
+        {
+			return false;
+        }
+
+		if(!Physics.Raycast(body.position, Vector3.down, out RaycastHit hit, probeDistance, probeMask)) //esto comprueba si hay suelo debajo, ya que si la condicion de arriba no se da, y entre dos superficies digamos que hay un vacio y esta pensado para que el jugador lo tenga que saltar, si solo es un frame
+														//la condicion de arriba nos lo tiraria para abajo, a pesar de que hay vacio. Entonces lo que comprueba eta funcion es si debajo de la bola hay algo con un rayo que devuelve si hay algo rollo rayosX en embarazo
+        {
+			return false;
+        }
+
+		if(hit.normal.y < GetMinDot(hit.collider.gameObject.layer)) //con lo que choca el raycast es retornado a traves de outRaycast, de esto podemos hcer el vector normal y calcular si entra dentro del rango que tenemos considerado que la bola esta en el aire o no.
+											   //En caso de que sea menor a ese angulo que consideramos, será que la pelota no esta en el suelo y se supone que tiene que estar en el aire, por tanto retornamos un false
+        {
+			return false;
+        }
+
+		//si no ha pasado nada de lo que hay arriba entonces hacemos snap de la pelota al suelo
+		groundContactCount = 1; //y como hacemos el snap al suelo le damos de valor 1 al groundContactcount, para que se reinicie en 1.
+		contactNormal = hit.normal;
+
+		//con estas 2 lineas lo que hacemos es ajsutar la velocidad al ground al que enganchamos la bola, funciona igual que lo que haciamos de velocidad deseada, pero aqui tenemos que mantener la velocidad que ya teniamos
+		float dot = Vector3.Dot(velocity, hit.normal); //asi que lo calculamos explicitamente en vez de usando la funcion ProjectOnContactPlane:
+		if(dot < 0f) //pero lo que está explicando encima solo haremos cuando dot sea positivo, es decir, cuando se de la situacion de que solo hay un frame fisico y se tiene que pegar la bola al suelo, que de normal la gravedad lo haría, pero habra en ocasioens que no
+			//y es cuando entonces lo forzamos nosotros
+        {
+		velocity = (velocity - hit.normal * dot).normalized * speed;
+        }
+		return true; 
+	}
+
 	void Jump()
     {
 		//velocity.y += 5f;  //En vez de hacer algo asi de simple, vamos a aplicar fisicas. Sabemos que en el tiro vertical la formula de la velocidad final es la raiz cuadrado de -2*gravedad*altura. Para saltar requerira que superemos la gravedad
-		if(onGround || jumpPhase < maxAirJumps) //si estamos en el suelo o solo hemos hecho un salto, ya que ahora permitiremos hacer dos saltos y el segundo puede ser estemos en el aire o donde sea.
+		//si estamos en el suelo o solo hemos hecho un salto, ya que ahora permitiremos hacer dos saltos y el segundo puede ser estemos en el aire o donde sea.
+
+			Vector3 jumpDirection;
+		if(onGround)
         {
+			jumpDirection = contactNormal;
+        }
+		else if (OnSteep)
+        {
+			jumpDirection = steepNormal;
+			jumpPhase = 0;
+        }
+		else if(maxAirJumps > 0 && jumpPhase <= maxAirJumps)
+        {
+			if(jumpPhase == 0)
+            {
+				jumpPhase = 1;
+            }
+			jumpDirection = contactNormal;
+        }
+        else
+        {
+			return;
+        }
+			stepsSinceLastJump = 0;
 			jumpPhase += 1;
 			float jumpSpeed = Mathf.Sqrt(-2f * Physics.gravity.y + jumpHeight);
-			float alignedSpeed = Vector3.Dot(velocity, contactNormal);
+			jumpDirection = (jumpDirection + Vector3.up).normalized; //esto es lo que nos permite escalar muros
+			float alignedSpeed = Vector3.Dot(velocity, /*contactNormal*/jumpDirection);
 
 			if(/*velocity.y*/alignedSpeed > 0f) //como queremos que cuando salte la velocidad sea la misma haga un vote o dos, tenemos que ahcer algunso cambios. Ya que sino como le vamos sumando el jumspeed, la velocidad en y y por tanto la distancia que puede moverse se va acumulando mucho, en vez de ser la misma por cada salto
             {
@@ -116,7 +205,7 @@ public class MovingSphere : MonoBehaviour
 
 			}
 
-			velocity += contactNormal * jumpSpeed; //Antes era velocity.y += jumspeed, pero ahora queremos que el salto pueda ser no solo vertical, sino segun la sueprficie que tocamos, asi que lo hacemos usando vector3. En el caso que este en el aire
+			velocity += /*contactNormal*/jumpDirection * jumpSpeed; //Antes era velocity.y += jumspeed, pero ahora queremos que el salto pueda ser no solo vertical, sino segun la sueprficie que tocamos, asi que lo hacemos usando vector3. En el caso que este en el aire
                                                   //contactNormal sería un vector.up que es : (0,1,0), así que un salto en el aire, si que solo nos moveria hacia arriba
 
 
@@ -124,7 +213,7 @@ public class MovingSphere : MonoBehaviour
                 "Spheres on steeper slopes no longer jump straight into their slopes but do get slowed down as the jump pushes " 
                 "them in the opposite direction that they're moving." */
 
-        }
+        
 
     }
 
@@ -141,6 +230,7 @@ public class MovingSphere : MonoBehaviour
 
 	void EvaluateCollision(Collision collision)
     {
+		float minDot = GetMinDot(collision.gameObject.layer);
         for (int i = 0; i < collision.contactCount; i++) //contactCount: Nos devuelve el numero de puntos de contacto que ha habido en la colision
         {
             Vector3 normal = collision.GetContact(i).normal; //GetContact devuelve el punto exacto de un indice. Y despues .normal calcula su normal (vector perpendicular).
@@ -166,13 +256,18 @@ public class MovingSphere : MonoBehaviour
 			//Obtendremos un escalar con el dotproduct, pongamos p.e 0,7, que lo tendremos guardado en la variable minGroundDotProduct. Por tanto cuando la normal.y sea menor a ese 0.7, onGround sera false y por tanto
 			//La aceleracion que se tendrá en cuenta será la de aire, lo que modificará la maxspeed a la que pueda ir lo que dificultará que se mueva por tanto, en el caso de las rampas, que pueda superarlas 
 
-			if(normal.y >= minGroundDotProduct)
+			if(normal.y >= minDot)
             {
 				groundContactCount += 1;
 				contactNormal += normal; //Antes era solo = normal, pero por si acaba la bola en un sitio con varias pendeintes y por tanto varios sitios donde colisiona,
 										 //lo que hacemos es acumular todos los vectores normales en contact normal y despues lo normalizamos en updatescene para que sea como si estuviera en un plano normal y no pete todo lo que hemos hecho
 										 //y la bola no se comporte raro
             } 
+			else if (normal.y > -0.01f)
+            {
+				steepContactCount += 1;
+				steepNormal += normal;
+            }
 
 			//Ahora queremos ^Hacer que los saltos varien segun el angulo, asi que lo que hacemos es conservar lo de que si la normal.y es mayor al minground, onground es true y ademas, guardamos en un vector3, 
 			//el vector normal que se da en ese punto en contacto entre bola y superficie
@@ -220,6 +315,25 @@ public class MovingSphere : MonoBehaviour
 
 		velocity += xAxis * (newX - currentX) + zAxis * (newZ - currentZ); //En vez de asignarle la velocidad nueva tal cual para que no parezca que se teletransporta hacemos la diferencia entre los dos vectores y lo multiplicamos por los normales
 	}
+
+	bool CheckSteepContacts()
+	{
+		if (steepContactCount > 1)
+		{
+			steepNormal.Normalize();
+			if (steepNormal.y >= minGroundDotProduct)
+			{
+				groundContactCount = 1;
+				contactNormal = steepNormal;
+				return true;
+			}
+		}
+		return false;
+	}
+
+
+	//returns whether it succeeded in converting the steep contacts into virtual ground.If there are multiple steep contacts then normalize them and check whether the result counts as ground.If so, return success, 
+	//otherwise failure.In this case we don't have to check for stairs
 
 
 }
